90bf8de5e8c1e54604684c925f92f2ea682289d1
João Pedro Augusto Costa <jpac1207@gmail.com>
2022-04-16 00:03:00 -0300

    Add function to split data in train/validation/test sets

--- mlp.m
+++ mlp.m
@@ -1,8 +1,8 @@
 H = 6; % Number of hidden layers
 I = 6; % Number of input layers
 O = 4; % Number of output layers
-eta = 0.00001; % Learning Rate
-maxEpochs = 5000; % Number of max epochs
+eta = 0.0001; % Learning Rate
+maxEpochs = 1000; % Number of max epochs
 acceptedError = 0.001; % Max accepted error
 activationType = 0; % 0 for sigmoid and 1 for tanh in the hidden layers
 
@@ -16,9 +16,10 @@ preProcessingConfig.labelMap = containers.Map({'unacc', 'acc', 'good', 'vgood'},
 
 data = readData('./data/car.data');
 [X, Y] = preProcessing(data, preProcessingConfig);
+[X_train, Y_train, X_val, Y_val, X_test, Y_test] = splitData(X, Y);
 %X
 
-[hiddenVsInputWeights, hiddenVsInputBias, outputVsHiddenWeights, outputVsHiddenBias]  = trainMLP(I, H, O, maxEpochs, eta, acceptedError, activationType, X', Y);
+%[hiddenVsInputWeights, hiddenVsInputBias, outputVsHiddenWeights, outputVsHiddenBias]  = trainMLP(I, H, O, maxEpochs, eta, acceptedError, activationType, X', Y);
 % prediction = testMLP(hiddenVsInputWeights, hiddenVsInputBias, outputVsHiddenWeights, outputVsHiddenBias, activationType, [1;1]);
 % sprintf("%f", prediction)
 % real = 1 & 1
@@ -54,19 +55,11 @@ function [hiddenVsInputWeights, hiddenVsInputBias, outputVsHiddenWeights, output
         Yh = activation(activationType, net_h);    
         % Output Layer
         net_o = Woh * Yh + bias_oh * ones(1, size (Yh, 2));        
-        Y_net = exp(net_o)./sum(exp(net_o));            
-        %errorMatrix = (Y .* (1-Y_net))
-        %E = (Y .* (1-Y_net))
-        %errorMatrix = ((-1)/size(Y_net, 2))*(sum(Y - Y_net,  'all'));
+        Y_net = exp(net_o)./sum(exp(net_o));                  
         E = (-1).*sum((Y.*log(Y_net)));    
 
         %E = (-1.*(sum((Y.*log(Y_net)), 'all')))./size(errorMatrix, 2)               
-        sprintf("%f", E);          
-        %sum((Y.*log(Y_net)))
-        %df = (-1).*(Y.*log(Y_net))
-        %E = (-1).*sum((Y.*log(Y_net)), "all")              
-        %df = (-1).*sum((Y.*log(Y_net)))
-        Y;
+        sprintf("%f", E);                 
         df =  (Y-Y_net);
         
         % backward    
@@ -103,14 +96,32 @@ function [hiddenVsInputWeights, hiddenVsInputBias, outputVsHiddenWeights, output
     outputVsHiddenBias = bias_oh;
 end
 
-function f = softmaxError(Y, Y_net)
-    errorMatrix = zeros(size(Y, 1), size(Y, 2));
-    for i = 1:size(Y,2)        
-        pos = find(Y(:, i) == 1);
-        errorMatrix(pos, i) = 1 - Y_net(pos, i);        
-    end
-    f = errorMatrix;
+function [X_train, Y_train, X_val, Y_val, X_test, Y_test] = splitData(X, Y)
+    numberOfRows = size(X, 1);
+    trainProportion = 0.7;
+    trainRows = floor(numberOfRows * trainProportion);
+    valProportion = 0.2;
+    valRows = floor(numberOfRows * valProportion);
+    testProportion = 0.1;
+    testRows = floor(numberOfRows * testProportion);
+
+    randIndexes = randperm(numberOfRows);
+    trainIndexes = randIndexes(1:trainRows)
+    initOfValRows = (trainRows + 1);
+    valIndexes = randIndexes(initOfValRows:(initOfValRows + valRows))
+    initOfTestRows = (initOfValRows + valRows + 1);
+    testIndexes = randIndexes(initOfTestRows:(initOfTestRows + testRows))
+
+    X_train = X(trainIndexes, :);
+    Y_train = Y(:, trainIndexes);
+    
+    X_val = X(valIndexes, :);
+    Y_val = Y(:, valIndexes);
+    
+    X_test = X(testIndexes, :);
+    Y_test = Y(:, testIndexes);
 end
+
 % This function applies the activation function on the parameter 'value'
 % according with the parameter 'type'
 function f = activation(type, value)

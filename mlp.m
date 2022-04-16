7fc7d76d84e8946863799a11dac3d41b070759bf
João Pedro Augusto Costa <jpac1207@gmail.com>
2022-04-15 22:11:14 -0300

    Add version with stable gradient

--- mlp.m
+++ mlp.m
@@ -1,15 +1,10 @@
-H = 4;
-I = 6;
-% O = 1;
-% eta = 0.1;
-% maxEpochs = 500;
-% acceptedError = 0.000001;
-% activationType = 0; % 0 for sigmoid and 1 for tanh
-% 
-% [hiddenVsInputWeights, hiddenVsInputBias, outputVsHiddenWeights, outputVsHiddenBias]  = trainMLP(I, H, O, maxEpochs, eta, acceptedError, activationType, X, Y);
-% prediction = testMLP(hiddenVsInputWeights, hiddenVsInputBias, outputVsHiddenWeights, outputVsHiddenBias, activationType, [1;1]);
-% sprintf("%f", prediction)
-% real = 1 & 1
+H = 6; % Number of hidden layers
+I = 6; % Number of input layers
+O = 4; % Number of output layers
+eta = 0.00001; % Learning Rate
+maxEpochs = 5000; % Number of max epochs
+acceptedError = 0.001; % Max accepted error
+activationType = 0; % 0 for sigmoid and 1 for tanh in the hidden layers
 
 preProcessingConfig.buyingMap = containers.Map({'vhigh', 'high', 'med', 'low'}, {5, 4, 3, 2});
 preProcessingConfig.maintMap = containers.Map({'vhigh', 'high', 'med', 'low'}, {5, 4, 3, 2});
@@ -21,7 +16,12 @@ preProcessingConfig.labelMap = containers.Map({'unacc', 'acc', 'good', 'vgood'},
 
 data = readData('./data/car.data');
 [X, Y] = preProcessing(data, preProcessingConfig);
-X
+%X
+
+[hiddenVsInputWeights, hiddenVsInputBias, outputVsHiddenWeights, outputVsHiddenBias]  = trainMLP(I, H, O, maxEpochs, eta, acceptedError, activationType, X', Y);
+% prediction = testMLP(hiddenVsInputWeights, hiddenVsInputBias, outputVsHiddenWeights, outputVsHiddenBias, activationType, [1;1]);
+% sprintf("%f", prediction)
+% real = 1 & 1
 
 function data = readData(dataPath)
     data = importdata(dataPath, ',');
@@ -53,12 +53,25 @@ function [hiddenVsInputWeights, hiddenVsInputBias, outputVsHiddenWeights, output
         net_h = Whi * X + bias_hi * ones(1, size(X, 2));
         Yh = activation(activationType, net_h);    
         % Output Layer
-        net_o = Woh * Yh + bias_oh * ones(1, size (Yh, 2));
-        Y_net = k * net_o;
-        E = Y - Y_net;
-             
+        net_o = Woh * Yh + bias_oh * ones(1, size (Yh, 2));        
+        Y_net = exp(net_o)./sum(exp(net_o));            
+        %errorMatrix = (Y .* (1-Y_net))
+        %E = (Y .* (1-Y_net))
+        %errorMatrix = ((-1)/size(Y_net, 2))*(sum(Y - Y_net,  'all'));
+        E = (-1).*sum((Y.*log(Y_net)));    
+
+        %E = (-1.*(sum((Y.*log(Y_net)), 'all')))./size(errorMatrix, 2)               
+        sprintf("%f", E);          
+        %sum((Y.*log(Y_net)))
+        %df = (-1).*(Y.*log(Y_net))
+        %E = (-1).*sum((Y.*log(Y_net)), "all")              
+        %df = (-1).*sum((Y.*log(Y_net)))
+        Y;
+        df =  (Y-Y_net);
+        
         % backward    
-        df = k * ones(size(net_o));   
+        %df = errorMatrix;   
+        %df
         delta_bias_oh = eta * sum((E.*df)')';
         delta_Woh = eta * (E.*df)*Yh';
         Eh = (Woh')*(E.*df);
@@ -73,8 +86,8 @@ function [hiddenVsInputWeights, hiddenVsInputBias, outputVsHiddenWeights, output
         Woh = Woh + delta_Woh;
         bias_oh = bias_oh + delta_bias_oh;        
    
-         %calculate error                  
-        error = sum(E.^2)/size(Y, 2);
+        %calculate error                          
+        error = sum(((Y .* (1-Y_net))), 'all')/size(Y, 2);        
         %sprintf("%f", error);
         errors(currentEpoch) = error;
        if(error < acceptedError)
@@ -90,6 +103,14 @@ function [hiddenVsInputWeights, hiddenVsInputBias, outputVsHiddenWeights, output
     outputVsHiddenBias = bias_oh;
 end
 
+function f = softmaxError(Y, Y_net)
+    errorMatrix = zeros(size(Y, 1), size(Y, 2));
+    for i = 1:size(Y,2)        
+        pos = find(Y(:, i) == 1);
+        errorMatrix(pos, i) = 1 - Y_net(pos, i);        
+    end
+    f = errorMatrix;
+end
 % This function applies the activation function on the parameter 'value'
 % according with the parameter 'type'
 function f = activation(type, value)
